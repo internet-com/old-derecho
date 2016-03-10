a99d30c3bb7280e50a5181c8d86943bfe6eb2a49
Sagar Jha <sjsagarjha3@gmail.com>
2016-03-10 17:53:10 -0500

    Small error in last commit :-(

--- derecho_group.h
+++ derecho_group.h
@@ -213,6 +213,7 @@ namespace derecho {
     // if the size of the message is greater than the buffer size, then return a -1 without thinking
     if (msg_size > buffer_size) {
       cout << "Can't send messages of size larger than the size of the circular buffer" << endl;
+      return -1;
     }
     long long int my_start = start[member_index];
     long long int my_end = end[member_index];
