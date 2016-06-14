ad5fe98865e4acc1088b95179f094040e5b90d65
Matthew Milano <milano@cs.cornell.edu>
2016-06-14 17:03:30 -0400

    remove old files

--- env.sh
+++
@@ -1,6 +0,0 @@
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH":/home/research/research/ken/derecho_caller/mutils/
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH":/home/research/research/ken/derecho_caller/mutils-serialization/
-export LIBRARY_PATH="$LIBRARY_PATH":"$LD_LIBRARY_PATH"
-
-export CPLUS_INCLUDE_PATH="$CPLUS_INCLUDE_PATH":/home/research/research/ken/derecho_caller/mutils/
-export CPLUS_INCLUDE_PATH="$CPLUS_INCLUDE_PATH":/home/research/research/ken/derecho_caller/mutils-serialization/
