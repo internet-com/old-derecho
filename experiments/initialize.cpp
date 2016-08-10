12245ed8401e8cfdb686b4a15da27f04ed325c4e
Sagar Jha <sjsagarjha3@gmail.com>
2016-08-10 19:08:29 -0400

    Small changes to initialize.h/cpp and CMakelists
    rdmc::initialize now returns a bool

--- experiments/initialize.cpp
+++ experiments/initialize.cpp
@@ -8,18 +8,22 @@
 #include "../rdmc/microbenchmarks.h"
 #include "../rdmc/group_send.h"
 
-void initialize (uint32_t &node_rank, uint32_t &num_nodes) {
+bool initialize (uint32_t &node_rank, uint32_t &num_nodes) {
   map<uint32_t, std::string> node_addresses;
 
   query_addresses(node_addresses, node_rank);
   num_nodes = node_addresses.size();
 
   // initialize RDMA resources, input number of nodes, node rank and ip addresses and create TCP connections
-  rdmc::initialize(node_addresses, node_rank);
+  if (!rdmc::initialize(node_addresses, node_rank)) {
+    return false;
+  }
 
   // initialize tcp connections
   sst::tcp::tcp_initialize(node_rank, node_addresses);
   
   // initialize the rdma resources
   sst::verbs_initialize();
+
+  return true;
 }
--- experiments/initialize.h
+++ experiments/initialize.h
@@ -3,5 +3,5 @@
 
 #include <cstdint>
 
-void initialize (uint32_t &node_rank, uint32_t &num_nodes);
+bool initialize (uint32_t &node_rank, uint32_t &num_nodes);
 #endif
